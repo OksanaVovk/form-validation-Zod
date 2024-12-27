a59d4b2fcd3e601d7817a885a045f65641b01cf1
Vovk Oksana <oksvovk85@gmail.com>
2024-12-27 12:40:47 +0200

    Code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { output: "export", basePath: "/typescript-next" };
 
 export default nextConfig;
--- src/app/schemas/bookingSchema.ts
+++ src/app/schemas/bookingSchema.ts
@@ -3,7 +3,7 @@ import { array, string, z } from "zod";
 
 export const bookingSchema = z.object({
     bookerName: z.string().min(2,{ message: "Booker name must be at least 2 characters long"}),
-      bookerEmail: z.string().optional().refine(val => val === '' || z.string().email().safeParse(val).success, {message: "Invalid email address"}),
+    bookerEmail: z.string().optional().refine(val => val === '' || z.string().email().safeParse(val).success, {message: "Invalid email address"}),
     eventName: z.string().min(2,{ message: "Event name must be at least 2 characters long"}),
     eventDate: z.date().min(new Date(), {message:"Event date must be in the future"}),
     numberOfGuests: z.number({required_error: "Number of Guests must be integer"}).int({message:"Number of Guests must be integer"}).min(1, {message: "Number of Guests must be at least 1"}).max(10, {message:"Number must be less than or equal to 10"}),
